cc0a517df63310e17362ff75ad700baaa086f234
Urvish Vekariya <urvishvekariya2001@gmail.com>
2023-08-18 19:32:41 +0530

    End points API created

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  return res.send("Hello world!");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  let first = req.body.first;
+  let second = req.body.second;
+
+  res.json({ result: first + second });
 });
 
+baseRouter.post("/subtract", (req, res) => {
+  let first = req.body.first;
+  let second = req.body.second;
 
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+  res.json({ result: first - second });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
